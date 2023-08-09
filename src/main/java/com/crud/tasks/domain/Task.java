c81849bddaec897a36969d3d0d2b075e517c5485
Wiktor Wozniakowski <wiktorwoz21@gmail.com>
2023-08-09 12:25:38 +0200

    error

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,10 +1,19 @@
 package com.crud.tasks.domain;
+import jakarta.persistence.*;
 import lombok.*;
 
-@Getter
+@NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Entity(name = "tasks")
 public class Task {
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "name")
     private String title;
+
+    @Column(name = "description")
     private String content;
 }
