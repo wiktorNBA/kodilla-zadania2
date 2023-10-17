7a8347288dd41d5e41e5677928223d2691900fdb
Wiktor Wozniakowski <wiktorwoz21@gmail.com>
2023-10-17 15:19:46 +0200

    22.3

--- src/main/java/com/crud/tasks/domain/AttachmentsByType.java
+++ src/main/java/com/crud/tasks/domain/AttachmentsByType.java
@@ -4,19 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class AttachmentsByType {
 
-    @JsonProperty("id")
-    private String id;
 
-    @JsonProperty("name")
-    private String name;
-
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
-
+    @JsonProperty(" trello ")
+    private Trello trello;
 }
--- src/main/java/com/crud/tasks/domain/Badges.java
+++ src/main/java/com/crud/tasks/domain/Badges.java
@@ -4,19 +4,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class Badges {
 
-    @JsonProperty("id")
-    private String id;
 
-    @JsonProperty("name")
-    private String name;
+    @JsonProperty(" votes ")
+    private int votes;
 
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
+    @JsonProperty(" attachmentsByType ")
+    private AttachmentsByType attachmentsByType;
 
 }
